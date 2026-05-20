55eabe33f8bcabf1b4b3a92ff149715db751a3a0
pointertobios <pointer-to-bios@outlook.com>
2026-05-21 00:11:53 +0800

    init

--- jungle-base/include/jungle/meta.h
+++ jungle-base/include/jungle/meta.h
@@ -5,8 +5,15 @@
 
 namespace jungle::meta {
 
+/**
+ * @brief 通过注解的类型判断类型是否有注解
+ * @tparam Anno
+ * @param type 被 Anno 值注解的类型反射信息
+ * @param _: Anno 值
+ * @return
+ */
 template<typename Anno>
-consteval bool has_annotation(const std::meta::info type, const Anno) {
+consteval bool has_annotation(const std::meta::info type, const Anno _) {
     if (std::meta::annotations_of_with_type(type, ^^Anno).size()) {
         return true;
     } else {
