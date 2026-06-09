f106866b5765ea62a8018001ce0b57c41f0ce157
pointertobios <pointertobios@outlook.com>
2026-06-09 17:50:49 +0800

    matrix

--- jungle-base/include/jungle/algebra/matrix.h
+++ jungle-base/include/jungle/algebra/matrix.h
@@ -104,7 +104,19 @@ public:
     }
 
     template<usize RLine, usize RRow>
-    matrix<T, Line * RLine, Row * RRow> kronecker_multiply(const matrix<T, RLine, RRow> &rhs) const {}
+    matrix<T, Line * RLine, Row * RRow> kronecker_multiply(const matrix<T, RLine, RRow> &rhs) const {
+        matrix<T, Line * RLine, Row * RRow> result{};
+        template for (constexpr auto i : std::views::iota(0u, Line)) {
+            template for (constexpr auto j : std::views::iota(0u, Row)) {
+                template for (constexpr auto k : std::views::iota(0u, RLine)) {
+                    template for (constexpr auto l : std::views::iota(0u, RRow)) {
+                        result[i * RLine + k, j * RRow + l] = (*this)[i, j] * rhs[k, l];
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
 private:
     std::array<T, Line * Row> m_data;
@@ -115,7 +127,7 @@ class matrix<T, N> : public matrix<T, N, N> {
 public:
     using matrix<T, N, N>::matrix;
 
-    static matrix identity() {
+    static consteval matrix identity() {
         matrix result{};
         template for (constexpr auto i : std::views::iota(0u, N)) { result[i, i] = scalar_const<T>::one; }
         return result;
@@ -144,5 +156,23 @@ public:
         requires(N == 3)
     {}
 };
+
+template<scalar_type T>
+using matrix3 = matrix<T, 3>;
+
+template<scalar_type T>
+using matrix4 = matrix<T, 4>;
+
+using matrix3f = matrix3<float>;
+using matrix4f = matrix4<float>;
+
+template<scalar_type T>
+using vector2 = vector<T, 2>;
+
+template<scalar_type T>
+using vector3 = vector<T, 3>;
+
+using vector2f = vector2<float>;
+using vector3f = vector3<float>;
 
 };  // namespace jungle::algebra
