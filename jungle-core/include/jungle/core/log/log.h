376610e4b85759d37e103466ef952e7b09a68076
pointertobios <pointer-to-bios@outlook.com>
2026-09-20 17:23:06 +0800

    log category

---
+++ jungle-core/include/jungle/core/log/log.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2026 pointer-to-bios <pointer-to-bios@outlook.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace jungle::core::log {
+
+enum class LogCategory {
+    Fatal,
+    Warning,
+    Info,
+    Debug,
+    Trace,
+};
+
+};
